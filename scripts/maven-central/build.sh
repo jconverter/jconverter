ff435a5bb0fa8563c797a154bb6cccb651f6e057
Sergio Castro <Sergio.Castro@tomtom.com>
2017-02-27 16:42:22 +0100

    updating the script for generating the bundle for mvn central

--- scripts/maven-central/build.sh
+++ scripts/maven-central/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 PROJECT=jconverter
-VERSION=0.0.1
+VERSION=0.0.2
+
+JAR_DIR=../../target
+cp $JAR_DIR/$PROJECT-$VERSION.jar $PROJECT-$VERSION.jar
+cp $JAR_DIR/$PROJECT-$VERSION-javadoc.jar $PROJECT-$VERSION-javadoc.jar
+cp $JAR_DIR/$PROJECT-$VERSION-sources.jar $PROJECT-$VERSION-sources.jar
+
+POM_DIR=../..
+cp $POM_DIR/pom.xml $PROJECT-$VERSION.pom
+
 gpg -ab $PROJECT-$VERSION.pom
 gpg -ab $PROJECT-$VERSION.jar
 gpg -ab $PROJECT-$VERSION-sources.jar
